fb72c7d3238bb704a387e45e182e6903edf8e7a6
Poojashree-19 <poojashreedh194@gmail.com>
2026-07-16 18:15:12 +0530

    Update instruction_memory_tb.sv

--- Enhanced_Single_Cycle_RV32I_Processor/instruction_memory_tb.sv
+++ Enhanced_Single_Cycle_RV32I_Processor/instruction_memory_tb.sv
@@ -2,50 +2,80 @@
 
 module instruction_memory_tb;
 
-   
+    // ---------------------------------------------
+    // Testbench Signals
+    // ---------------------------------------------
+
     logic [31:0] address;
     logic [31:0] instruction;
 
-    instruction_memory dut (
+    // ---------------------------------------------
+    // Instantiate DUT
+    // ---------------------------------------------
+
+    instruction_memory dut(
+
         .address(address),
         .instruction(instruction)
+
     );
 
+    // ---------------------------------------------
+    // Monitor
+    // ---------------------------------------------
+
     initial begin
+
         $monitor("Time=%0t | Address=%0d | Instruction=%h",
-                 $time, address, instruction);
+                  $time, address, instruction);
+
     end
 
-    
+    // ---------------------------------------------
+    // Test Cases
+    // ---------------------------------------------
 
     initial begin
 
         $dumpfile("instruction_memory.vcd");
         $dumpvars(0, instruction_memory_tb);
 
-        $display("======================================");
+        $display("========================================");
         $display("Starting Instruction Memory Test");
-        $display("======================================");
+        $display("========================================");
 
-        // Test Case 1 : Fetch Instruction at Address 0
+        // Instruction 0
         address = 32'd0;
         #10;
 
-        // Test Case 2 : Fetch Instruction at Address 4
+        // Instruction 1
         address = 32'd4;
         #10;
 
-        // Test Case 3 : Fetch Instruction at Address 8
+        // Instruction 2
         address = 32'd8;
         #10;
 
-        // Test Case 4 : Fetch Instruction at Address 12
+        // Instruction 3
         address = 32'd12;
         #10;
 
-        $display("======================================");
+        // NOP Instructions
+        address = 32'd16;
+        #10;
+
+        address = 32'd20;
+        #10;
+
+        address = 32'd24;
+        #10;
+
+        address = 32'd28;
+        #10;
+
+        $display("========================================");
         $display("Instruction Memory Test Completed");
-        $display("======================================");
+        $display("========================================");
 
         $finish;
 
