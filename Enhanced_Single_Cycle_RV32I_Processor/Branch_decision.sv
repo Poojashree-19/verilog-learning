c5dec801cc468a70c46d2f85efb7b6562933ebfa
Poojashree-19 <poojashreedh194@gmail.com>
2026-07-17 12:32:41 +0530

    Update Branch_decision.sv

--- Enhanced_Single_Cycle_RV32I_Processor/Branch_decision.sv
+++ Enhanced_Single_Cycle_RV32I_Processor/Branch_decision.sv
@@ -1,12 +1,56 @@
 module branch_decision(
 
-    input  logic Branch,
-    input  logic Zero,
+    input logic        Branch,
 
-    output logic PCSrc
+    input logic [2:0]  funct3,
+
+    input logic        Zero,
+    input logic        less_than,
+    input logic        less_than_u,
+
+    output logic       PCSrc
 
 );
 
-    assign PCSrc = Branch & Zero;
+always_comb begin
+
+    PCSrc = 1'b0;
+
+    if (Branch) begin
+
+        case (funct3)
+
+            // BEQ
+            3'b000:
+                PCSrc = Zero;
+
+            // BNE
+            3'b001:
+                PCSrc = ~Zero;
+
+            // BLT
+            3'b100:
+                PCSrc = less_than;
+
+            // BGE
+            3'b101:
+                PCSrc = ~less_than;
+
+            // BLTU
+            3'b110:
+                PCSrc = less_than_u;
+
+            // BGEU
+            3'b111:
+                PCSrc = ~less_than_u;
+
+            default:
+                PCSrc = 1'b0;
+
+        endcase
+
+    end
+
+end
 
 endmodule
